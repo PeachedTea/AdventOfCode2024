567a7cf5c800de021a9733e6fc3694e136dd185c
Caden Fehr <cadenfehr@yahoo.ca>
2024-12-01 00:02:05 -0600

    Solved part2! Might clean up inefficiencies.

--- Dec1st.py
+++ Dec1st.py
@@ -20,3 +20,22 @@ def list_distance_solver():
     return total
 
 print(list_distance_solver())
+
+def simimlarity_score():
+    similarity = 0
+    i = 0
+    masterlist = [[], []]
+    puzzleInput = open("input2.txt", "r")
+    for line in puzzleInput:
+        list1 = line.split('   ')
+        masterlist[0].append(int(list1[0]))
+        masterlist[1].append(int(list1[1]))
+        i += 1
+    puzzleInput.close()
+    # super inefficent but works
+    for i in range(len(masterlist[0])):
+        similarity += masterlist[0][i] * (masterlist[1].count(masterlist[0][i]))
+
+    return similarity
+
+print(simimlarity_score())
